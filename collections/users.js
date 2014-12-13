efff77da1bc63ab3ab78f00dccfe16b7e4af9736
skv <skv-headless@yandex.ru>
2014-12-13 13:08:21 +0300

    bet and account

--- collections/users.js
+++ collections/users.js
@@ -7,3 +7,51 @@ Meteor.users.allow({
   },
   fetch: ['_id']
 });
+
+UserProfile = new SimpleSchema({
+  room_id: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    optional: true
+  },
+  name: {
+    type: String
+  },
+  account: {
+    type: Number,
+    defaultValue: 100
+  },
+  bet: {
+    type: Number,
+    defaultValue: 0
+  }
+});
+
+User = new SimpleSchema({
+    emails: {
+        type: [Object],
+        optional: true
+    },
+    "emails.$.address": {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email
+    },
+    "emails.$.verified": {
+        type: Boolean
+    },
+    createdAt: {
+        type: Date
+    },
+    profile: {
+        type: UserProfile,
+        optional: true
+    },
+    services: {
+        type: Object,
+        optional: true,
+        blackbox: true
+    }
+});
+
+Meteor.users.attachSchema(User);
+
