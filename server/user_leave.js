73e26fab37c2ac22017faac1368186a65d02fe68
skv <skv-headless@yandex.ru>
2014-12-13 21:58:29 +0300

    remove room after owner leave

--- server/user_leave.js
+++ server/user_leave.js
@@ -1,7 +1,13 @@
 Meteor.methods({
-  userLeave: function(doc) {
-    Meteor.users.update({_id: doc._id},
-                        {$unset: {'profile.room_id': 1}})
+  userLeave: function(user) {
+    var room = Rooms.findOne({_id: user.profile.room_id});
+
+    if( room && room.owner_id == user._id) {
+      Rooms.remove({_id: room._id});
+    } else {
+      Meteor.users.update({_id: user._id},
+                          {$unset: {'profile.room_id': 1}})
+    }
   }
 })
 
